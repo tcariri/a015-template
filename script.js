3dae74169187dec62f82273b4933bcabf13b24a7
tcariri <thais.cariri2016@gmail.com>
2022-09-13 20:48:39 -0400

    atualizando

--- script.js
+++ script.js
@@ -4,12 +4,25 @@
 // um conjunto de ações
 // uma alteração 
 
-let numero = +prompt("digite um número para somar ou 0 para sair")
-let soma=0
-while (numero!==0 ) {
-soma=soma+numero
-numero = +prompt("digite um número para somar ou 0 para sair")
+
+//pratica guiada I
+// let numero = +prompt("digite um número para somar ou 0 para sair")
+// let soma=0
+// while (numero!==0 ) {
+// soma=soma+numero
+// numero = +prompt("digite um número para somar ou 0 para sair")
+// }
+
+// console.log(soma);
+
+
+//pratica guiada II
+const numero = +prompt("digite um número ")
+
+for(let i=0;i<=numero;i=i+1){
+    console.log(i)
 }
 
-console.log(soma);
+
+
 
